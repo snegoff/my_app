b2d016ffc836af75e6fc2c310f178d7f98ab2886
SNEG <sneg@snegbklinux.Dlink>
2012-11-06 22:46:53 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyApp::Application.config.secret_token = '293fcd1d1f6799b20240a8770524727325b2da6d334e542596e0dbda86dd0dd711181ebcdff2dc5c5c836c2d9837a882afa9eb748ae37cac387507d66c13fa0e'
